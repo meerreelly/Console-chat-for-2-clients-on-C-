fa3ed5963b8ac35dc813b23a72b01ab12ddaeff8
merely <merely@DESKTOP-PRBVCO8>
2024-03-19 01:28:16 +0200

    Add project files.

---
+++ chat/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    static TcpListener listener;
+    static Socket client;
+    static Socket client2;
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Server started...");
+        // Start listening on port
+        listener = new TcpListener(IPAddress.Any, 888);
+        listener.Start();
+        // Accept client connections
+        client = listener.AcceptSocket();
+        client2 = listener.AcceptSocket();
+        // Start a new thread for receiving messages from the client
+        Thread receiverThread = new Thread(ReceiveMessage1);
+        Thread receiverThread2 = new Thread(ReceiveMessage2);
+        receiverThread.Start();
+        receiverThread2.Start();
+        // Loop to send messages to the client
+        string message;
+        while (true)
+        {
+            message = Console.ReadLine();
+            SendMessage("Server: "+message, client);
+            SendMessage("Server: "+message, client2);
+        }
+    }
+
+   static void ReceiveMessage1()
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while (true)
+        {
+            bytesRead = client.Receive(buffer);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            SendMessage(Encoding.ASCII.GetString(buffer, 0, bytesRead), client2);
+        }
+    }
+    static void ReceiveMessage2()
+    {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while (true)
+        {
+            bytesRead = client2.Receive(buffer);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            SendMessage(Encoding.ASCII.GetString(buffer, 0, bytesRead), client);
+        }
+    }
+    static void SendMessage(string message, Socket client)
+    {
+        byte[] buffer = Encoding.ASCII.GetBytes(message);
+        client.Send(buffer);
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static TcpClient client;
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Client started...");
+        // Connect to the server on localhost and port 8888
+        client = new TcpClient("localhost", 888);
+        // Start a new thread for receiving messages from the server
+        System.Threading.Thread receiverThread = new System.Threading.Thread(ReceiveMessage);
+        receiverThread.Start();
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        // Loop to send messages to the server
+        string message;
+        while (true)
+        {
+            
+            message = Console.ReadLine();
+            SendMessage(name+": "+message);
+        }
+    }
+    static void ReceiveMessage()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+        while (true)
+        {
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        }
+    }
+
+    static void SendMessage(string message)
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = Encoding.ASCII.GetBytes(message);
+        nwStream.Write(buffer, 0, buffer.Length);
+        nwStream.Flush();
+    }
+}
